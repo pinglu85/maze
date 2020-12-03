be757e584b7444cbbadf42f77e0fbbceae589b19
Ping Lu <luping8576@hotmail.com>
2020-12-03 18:10:56 +0100

    initial commit

--- js/constants/colors.js
+++ js/constants/colors.js
@@ -3,6 +3,7 @@ export const CELL_COLORS = {
   start: '#57b3f9',
   scanning: '#7cefb0',
   visited: '#ffffff',
-  unvisited: '#d3d3d3'
+  unvisited: '#d3d3d3',
+  solution: '#ff9f32'
 };
 export const SYMBOL_COLOR = '#c675ff';
