75103e753e464bbde81cc346f2d9cad091fdab35
pinglu85 <luping8576@hotmail.com>
2020-12-19 16:53:47 +0100

    refactor: split the function '_recursiveDivision' into three functions

--- js/mazeGenerationAlgos/recursiveDivision.js
+++ js/mazeGenerationAlgos/recursiveDivision.js
@@ -1,52 +1,51 @@
 import { getRandomIndex } from '../utils/index.js';
 
-function _recursiveDivision(grid, startRow, endRow, startCol, endCol) {
+function divideHorizontally(grid, startRow, endRow, startCol, endCol) {
+  const mid = Math.floor((startRow + endRow) / 2);
+  const escapedCellColIndex = getRandomIndex(endCol - startCol + 1) + startCol;
+  for (let i = startCol; i <= endCol; i++) {
+    if (i === escapedCellColIndex) {
+      continue;
+    }
+
+    grid[mid][i].addWall('south');
+    grid[mid + 1][i].addWall('north');
+  }
+
+  divide(grid, startRow, mid, startCol, endCol);
+  divide(grid, mid + 1, endRow, startCol, endCol);
+}
+
+function divideVertically(grid, startRow, endRow, startCol, endCol) {
+  const mid = Math.floor((startCol + endCol) / 2);
+  const escapedCellRowIndex = getRandomIndex(endRow - startRow + 1) + startRow;
+  for (let i = startRow; i <= endRow; i++) {
+    if (i === escapedCellRowIndex) {
+      continue;
+    }
+
+    grid[i][mid].addWall('west');
+    grid[i][mid + 1].addWall('east');
+  }
+
+  divide(grid, startRow, endRow, startCol, mid);
+  divide(grid, startRow, endRow, mid + 1, endCol);
+}
+
+function divide(grid, startRow, endRow, startCol, endCol) {
   if (startRow === endRow || startCol === endCol) {
     return;
   }
 
-  let mid, isHorizontalDivision;
   if (endRow - startRow + 1 > endCol - startCol + 1) {
-    isHorizontalDivision = true;
+    divideHorizontally(grid, startRow, endRow, startCol, endCol);
   } else {
-    isHorizontalDivision = false;
-  }
-
-  if (isHorizontalDivision) {
-    mid = Math.floor((startRow + endRow) / 2);
-    const escapedCellColIndex =
-      getRandomIndex(endCol - startCol + 1) + startCol;
-    for (let i = startCol; i <= endCol; i++) {
-      if (i === escapedCellColIndex) {
-        continue;
-      }
-
-      grid[mid][i].addWall('south');
-      grid[mid + 1][i].addWall('north');
-    }
-
-    _recursiveDivision(grid, startRow, mid, startCol, endCol);
-    _recursiveDivision(grid, mid + 1, endRow, startCol, endCol);
-  } else {
-    mid = Math.floor((startCol + endCol) / 2);
-    const escapedCellRowIndex =
-      getRandomIndex(endRow - startRow + 1) + startRow;
-    for (let i = startRow; i <= endRow; i++) {
-      if (i === escapedCellRowIndex) {
-        continue;
-      }
-
-      grid[i][mid].addWall('west');
-      grid[i][mid + 1].addWall('east');
-    }
-
-    _recursiveDivision(grid, startRow, endRow, startCol, mid);
-    _recursiveDivision(grid, startRow, endRow, mid + 1, endCol);
+    divideVertically(grid, startRow, endRow, startCol, endCol);
   }
 }
 
 function recursiveDivision(grid) {
-  _recursiveDivision(grid, 0, grid.length - 1, 0, grid[0].length - 1);
+  divide(grid, 0, grid.length - 1, 0, grid[0].length - 1);
   return false;
 }
 
