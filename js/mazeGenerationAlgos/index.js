d3ba04d6f13fa1e76b8307da4bd46f8629ba7c2d
pinglu85 <luping8576@hotmail.com>
2020-12-19 16:04:43 +0100

    refactor: create index file for mazeGenerationAlgos

---
+++ js/mazeGenerationAlgos/index.js
@@ -0,0 +1,5 @@
+export { default as asyncAldousBroderAlgo } from './aldousBroderAlgo.js';
+export { default as asyncBinaryTree } from './binaryTree.js';
+export { default as asyncHuntAndKill } from './huntAndKill.js';
+export { default as asyncRecursiveBacktracker } from './recursiveBacktracker.js';
+export { default as recursiveDivision } from './recursiveDivision.js';
