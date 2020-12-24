2e31c876de89904d224b8c6212febb0a95180d79
pinglu85 <luping8576@hotmail.com>
2020-12-24 16:20:52 +0100

    adjust colors

--- js/constants/colors.js
+++ js/constants/colors.js
@@ -4,17 +4,17 @@ export const CELL_COLORS = {
   scanning: '#7cefb0',
   visited: 'transparent',
   unvisited: '#d3d3d3',
-  connected: '#e8e8e8',
+  connected: '#ededed',
   neighbor: '#bfecff',
   sameSet: '#fcccd9',
   differentSet: '#c8ffc4',
   pathfinding: {
-    visited: '#94d6fc',
-    visiting: '#4fb8ff',
-    exitColor: '#067ece',
+    visited: '#d0c9fc',
+    visiting: '#9586f9',
+    exitColor: '#6d2aea',
   },
 };
 export const FOOTPRINT_COLORS = {
   newFootprint: '#ffcc01',
-  oldFootprint: '#c675ff',
+  oldFootprint: '#9c45ed',
 };
