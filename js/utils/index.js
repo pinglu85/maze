6d861b85ddf49a48d342548ea1b205d76b85ec37
pinglu85 <luping8576@hotmail.com>
2020-12-30 19:19:04 +0100

    refactor: move setDefaultGridSize to ui folder

--- js/utils/index.js
+++ js/utils/index.js
@@ -4,6 +4,5 @@ export { default as getOppositeDir } from './getOppositeDir.js';
 export { default as getRandomIndex } from './getRandomIndex.js';
 export { default as getStartOrEndIndexOfArray } from './getStartOrEndIndexOfArray.js';
 export { loadStartNodeSprites, loadTargetNodeSprites } from './loadSprites.js';
-export { default as setDefaultGridSize } from './setDefaultGridSize.js';
 export { shuffleArr, shuffleArrIndices } from './shuffleArray.js';
 export { default as swapItemsInArray } from './swapItemsInArray.js';
