cadbd32e494ac61082797fa42f78f19e1c6c7432
pinglu85 <luping8576@hotmail.com>
2021-01-21 19:33:23 +0100

    refactor: move the creation of store to the main index file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-import { createElement, render, setInitialGridSize } from './utils';
-import store from './store';
+import {
+  createElement,
+  render,
+  createStore,
+  setInitialGridSize,
+} from './utils';
+import appStateReducer from './store/appStateReducer';
 import App from './App';
 import './index.css';
+
+const initialAppState = {
+  gridSize: {
+    numOfRows: 0,
+    numOfCols: 0,
+  },
+  canvasSize: {
+    width: 0,
+    height: 0,
+  },
+  algoType: '',
+  mazeAlgo: '',
+  pathfindingAlgo: '',
+  isSettingsDrawerOpen: false,
+  popupWarning: {
+    isShown: false,
+    message: '',
+  },
+  isMazeGenerating: false,
+  isMazeGenerated: false,
+  isSearchingSolution: false,
+  isSolutionFound: false,
+};
+
+const store = createStore(appStateReducer, initialAppState);
 
 const rootElement = document.getElementById('root');
 const node = render(<App store={store} />);
