5d03b8a2c931e17343388778139f6ee792218a23
pinglu85 <luping8576@hotmail.com>
2021-01-25 17:37:47 +0100

    refactor: rename text textContent.

--- src/Navbar/PathfindingAlgosDropdown/index.js
+++ src/Navbar/PathfindingAlgosDropdown/index.js
@@ -3,8 +3,11 @@ import Dropdown from '../../shared/Dropdown';
 import { PATHFINDING_ALGO_IDS } from '../../constants/algoIds';
 
 const algoNames = [
-  { id: PATHFINDING_ALGO_IDS.dijkstrasAlgo, text: "Dijkstra's Algorithm" },
-  { id: PATHFINDING_ALGO_IDS.aStarSearch, text: 'A* Search' },
+  {
+    id: PATHFINDING_ALGO_IDS.dijkstrasAlgo,
+    textContent: "Dijkstra's Algorithm",
+  },
+  { id: PATHFINDING_ALGO_IDS.aStarSearch, textContent: 'A* Search' },
 ];
 
 const PathfindingAlgosDropdown = ({ store }) => {
