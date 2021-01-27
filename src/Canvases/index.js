778cf5c5cee9b349de5bb7a865f7d9d82e284d3e
pinglu85 <luping8576@hotmail.com>
2021-01-27 22:36:50 +0100

    refactor: adjust the folder structure.

--- src/Canvases/index.js
+++ src/Canvases/index.js
@@ -1,6 +1,6 @@
 import { createElement, useRef } from '../utils';
 import { gridSizeUpdated } from '../constants/actionTypes';
-import Canvas from '../sharedComponents/Canvas';
+import Canvas from './Canvas';
 import grid from '../Grid';
 import setCanvasesSize from './setCanvasesSize';
 import styles from './style.module.css';
