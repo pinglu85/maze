df3e881cffbd31ba65ed5f71d8bcf464e872562f
pinglu85 <luping8576@hotmail.com>
2021-02-01 18:03:15 +0100

    refactor: use priority queue.

--- src/Grid/pathfindingAlgos/aStarSearch.js
+++ src/Grid/pathfindingAlgos/aStarSearch.js
@@ -1,34 +1,43 @@
 import reconstructPath from './utils/reconstructPath';
+import PriorityQueue from './utils/PriorityQueue';
 
 async function asyncAStarSearch(grid, entranceCell, exitCell, wait = 50) {
-  const openList = [entranceCell];
-  const closedList = [];
+  const pq = new PriorityQueue((cellA, cellB) => cellA.f - cellB.f);
+  pq.add(entranceCell);
   entranceCell.isInOpenList = true;
   entranceCell.g = 0;
   entranceCell.f = 0;
 
-  while (openList.length > 0) {
-    await asyncGetSuccessors(grid, exitCell, openList, closedList, wait);
+  const closedList = [];
+
+  while (pq.size() > 0) {
+    const q = pq.poll();
+    q.isInOpenList = false;
+
+    if (q.isExit) {
+      q.isExitColor = true;
+      break;
+    }
+
+    closedList.push(q);
+    q.isInClosedList = true;
+    q.opacity = 0.8;
+
+    await asyncGetSuccessors(q, grid, exitCell, pq, wait);
   }
 
   return reconstructPath(exitCell);
 }
 
-function asyncGetSuccessors(grid, exitCell, openList, closedList, wait) {
+function asyncGetSuccessors(q, grid, exitCell, openList, wait) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      getSuccessors(grid, exitCell, openList, closedList, resolve);
+      getSuccessors(q, grid, exitCell, openList, resolve);
     }, wait);
   });
 }
 
-function getSuccessors(grid, exitCell, openList, closedList, resolve) {
-  const q = openList.sort((cellA, cellB) => cellB.f - cellA.f).pop();
-  q.isInOpenList = false;
-  closedList.push(q);
-  q.isInClosedList = true;
-  q.opacity = 0.8;
-
+function getSuccessors(q, grid, exitCell, openList, resolve) {
   const successors = q.getConnectedNeighbors(grid);
 
   for (const successor of successors) {
@@ -36,27 +45,19 @@ function getSuccessors(grid, exitCell, openList, closedList, resolve) {
       continue;
     }
 
-    successor.parent = q;
-
-    if (successor.isExit) {
-      successor.isExitColor = true;
-      openList.length = 0;
-      resolve();
-      return;
-    }
-
     const newG = q.g + 1;
     const newH = computeManhattanDistance(successor, exitCell);
     const newF = newG + newH;
 
-    if (successor.f > newF) {
-      if (!successor.isInOpenList) {
-        openList.push(successor);
-        successor.isInOpenList = true;
-      }
+    if (newF < successor.f) {
       successor.g = newG;
       successor.h = newH;
       successor.f = newF;
+      successor.parent = q;
+      if (!successor.isInOpenList) {
+        openList.add(successor);
+        successor.isInOpenList = true;
+      }
     }
   }
 
@@ -65,8 +66,8 @@ function getSuccessors(grid, exitCell, openList, closedList, resolve) {
 
 function computeManhattanDistance(currCell, goal) {
   return (
-    Math.abs(currCell.colIndex - goal.colIndex) +
-    Math.abs(currCell.rowIndex - goal.rowIndex)
+    Math.abs(currCell.centerX - goal.centerX) +
+    Math.abs(currCell.centerY - goal.centerY)
   );
 }
 
